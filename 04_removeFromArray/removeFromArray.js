b482b10aa6e4eeb8ee91370ec211d9663c3b822c
Antoni Piryankov <apiryankov@gmail.com>
2024-04-29 16:15:57 +0300

    Add solutions one through five

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
-
+	let array = arguments[0];
+	let reduce_arr = Array.from(arguments);
+	let new_arr = [];
+	reduce_arr.shift();
+	for (let i = 0; i<array.length; i++) {
+		let is_unique = true;
+		for (let j = 0; j<reduce_arr.length; i++) {
+			if (array[i] === reduce_arr[j]) {
+				is_unique = false;
+			}
+		}
+		if ( is_unique ) {
+			new_arr.push(array[i]);
+		}
+	}
+	return new_arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+	if ( num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number" ) {
+		return 'ERROR';
+	}
+
+	const count = Math.abs(num2 - num1);
+	let sum = 0;
+	let current = Math.min(num1, num2);
+
+	for ( let i = 0; i <= count; i++ ) {
+		
+		sum += current;
+		current ++;
+	}
+	return sum;
 };
 
 // Do not edit below this line
