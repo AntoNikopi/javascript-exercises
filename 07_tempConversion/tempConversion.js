19bff03c377d61aea993319acea6c2e2c3414637
Antoni Piryankov <apiryankov@gmail.com>
2024-04-30 09:52:22 +0300

    Add tasks 06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesC) {
+	let degreesF = ((degreesC -32) * 5 )/ 9;
+	let degreesRounded = degreesF.toFixed(1);
+	return parseFloat(degreesRounded);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesF) {
+	let degreesC = ( degreesF * 9 ) / 5 + 32;
+	let degreesRounded = degreesC.toFixed(1);
+	return parseFloat(degreesRounded);
 };
 
 // Do not edit below this line
